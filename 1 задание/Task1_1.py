56965dbe96aad2c1ff65411d8af4ecca2d27c69b
yakushev42 <georgyyakushev04@yandex.ru>
2024-03-31 02:13:25 +0300

    Наброски

--- 1 задание/Task1_1.py
+++ 1 задание/Task1_1.py
@@ -1,15 +1,10 @@
 import time
 import threading
+import multiprocessing
 from piosdk.piosdk import Pioneer
-from sklearn.cluster import KMeans  # pip install scikit-learn
+#from sklearn.cluster import KMeans  # pip install scikit-learn
 import dataclasses
-
-
-def get_center_clusters(data, num_clasters):
-    """ Поиск кластеров в наборе данных """
-    kmeans = KMeans(n_clusters=num_clasters, max_iter=100)
-    kmeans.fit(data)
-    return kmeans.cluster_centers_
+from typing import List
 
 # Классы для хранения настроек подключения
 @dataclasses.dataclass
@@ -32,119 +27,180 @@ class RobotConnectingData:
     robot3: IpPort = IpPort(ip="127.0.0.1", port=8007)
 
 
-drones = []
-#Иницииализация дронов
-drones.append(Pioneer(ip=DroneConnectingData.drone0.ip, mavlink_port=DroneConnectingData.drone0.port))
-drones.append(Pioneer(ip=DroneConnectingData.drone1.ip, mavlink_port=DroneConnectingData.drone1.port))
-drones.append(Pioneer(ip=DroneConnectingData.drone2.ip, mavlink_port=DroneConnectingData.drone2.port))
-drones.append(Pioneer(ip=DroneConnectingData.drone3.ip, mavlink_port=DroneConnectingData.drone3.port))
+class Point:
+    def __init__(self, x: float, y: float, z: float):
+        self.x = x
+        self.y = y
+        self.z = z
+    x: float = 0
+    y: float = 0
+    z: float = 0
+    fire: bool = False
 
 
-centre_fire = []  # массив куда запишутся центры пожаров
+def intermediate_points(point_a: Point, point_b: Point, step_size:float = 1) -> List[Point]:
+    """
+    Функция для расчёта промежуточных точек, чтобы траектория была более ровной
+    Из точки А в точку Б, step-size - расстояние между шагами
+    Возвращает список точек, включая А и Б
+    """
+    # Разность координат между точками
+    dx = point_b.x - point_a.x
+    dy = point_b.y - point_a.y
+
+    # Определяем длину отрезка между точками
+    length = ((dx ** 2) + (dy ** 2)) ** 0.5
+
+    # Вычисляем количество шагов
+    steps = int(length / step_size)
+
+    if steps == 0:
+        steps = 1
+
+    # Шаг изменения координаты
+    step_x = dx / steps
+    step_y = dy / steps
+
+    # Генерация промежуточных точек
+    intermediate = []
+    # intermediate.append(point_a)
+
+    for i in range(steps):
+        x = point_a.x + i * step_x
+        y = point_a.y + i * step_y
+        intermediate.append((Point(round(x, 2), round(y, 2), point_b.z)))
+    intermediate.append(point_b)
+
+    return intermediate
+
+
+class Drone:
+    def __init__(self, ip: str, port: int):
+        self.m_drone = Pioneer(ip=ip, mavlink_port=port)
+        self.m_parolPoints = []
+        self.m_homePoint = Point(0,0,0)
+        self.m_eshelon = 2
+
+    def setEshelon(self, height: float):
+        self.m_eshelon = height
+    def checkFire(self) -> bool:
+        """
+        Проверка на наличие огня
+        True - False
+        """
+        temp = self.m_drone.get_piro_sensor_data()
+        sec = 5
+        while sec:
+            if (temp is not None) and (temp > 40):
+                return True
+            time.sleep(1)
+            sec = sec - 1
+        return False
+
+    def getCurrentPos(self) -> Point:
+        """
+        Получение текущей позиции дрона
+        """
+        current = None
+        while current is None:
+            current = self.m_drone.get_local_position_lps()
+        return Point(current[0], current[1], current[1])
+    def goToPoint(self, point: Point):
+        """
+        Лететь в точку
+        """
+        self.m_drone.go_to_local_point(point.x, point.y, self.m_eshelon)
+        while not self.m_drone.point_reached():
+            time.sleep(0.5)
+    def goToPointStraight(self, point: Point):
+        """
+        лететь в точку с промежуточными точками
+        """
+        currentPos = self.getCurrentPos()
+        route = intermediate_points(currentPos, point)
+        for step in route:
+            self.goToPoint(step)
+
+    def addPatrolPoint(self, point: Point):
+        """
+        Добавить точку патрулирования
+        """
+        self.m_parolPoints.append(point)
+    def startPatrol(self):
+        """
+        Бесконечное патрулирование точек
+        """
+        self.m_drone.arm()
+        self.m_drone.takeoff()
+        while True:
+            for point in self.m_parolPoints:
+
+                self.goToPointStraight(point)
+                fire = self.checkFire()
+                point.fire = fire
+            time.sleep(1)
+
+    m_drone: Pioneer
+    m_parolPoints: List[Point] = []
+    m_homePoint: Point
+    m_eshelon: float
+
+drones: List[Drone] = []
+
+drones.append(Drone(ip = DroneConnectingData.drone0.ip, port = DroneConnectingData.drone0.port))
+drones.append(Drone(ip = DroneConnectingData.drone1.ip, port = DroneConnectingData.drone1.port))
+drones.append(Drone(ip = DroneConnectingData.drone2.ip, port = DroneConnectingData.drone2.port))
+drones.append(Drone(ip = DroneConnectingData.drone3.ip, port = DroneConnectingData.drone3.port))
+
 start_position_dron2 = [-1.09, -4.76, 2]
 start_position_dron1 = [-0.25, -4.76, 2]
 #массив координат полета, где есть пожар
 mission_point = [
-        [-3.85, -1.06, 2],  # fire
-        [-0.86, -1.92, 2],
-        [2.02, -1.29, 2],
-        [1.23, 0.20, 2],
-        [0.68, 1.50, 2],  # fire
-        [-0.64, 2.53, 2],
-        [-2.39, 2.95, 2],
-        [3.40, 2.04, 2],
-        [2.28, 3.60, 2],
-        [-4.81, 4.36, 2],
+        Point(-4, -4, 2),  # fire
+        Point(-4, -0, 2),
+        Point(0, -4, 2),
+        Point(-4, 0, 2),
+        Point(0, 0, 2),  # fire
+        Point(0, 2, 2),
+        Point(-4, 4, 2),
+        Point(-4, 2, 2),
+        Point(-2, 2, 2),
+        Point(-3, 3, 2)
     ]
-def dron1_task():
-    drones[2].arm()
-    drones[2].takeoff()
-    drones[2].go_to_local_point(x=mission_point[0][0],
-                                y=mission_point[0][1],
-                                z=mission_point[0][2])
-    while not drones[2].point_reached():
-        pass
-    time.sleep(5)
-    drones[2].go_to_local_point(x=mission_point[1][0],
-                                y=mission_point[1][1],
-                                z=mission_point[1][2])
-    while not drones[2].point_reached():
-        pass
-    time.sleep(5)
-    drones[2].go_to_local_point(x=mission_point[2][0],
-                                y=mission_point[2][1],
-                                z=mission_point[2][2])
-    while not drones[2].point_reached():
-        pass
-    time.sleep(5)
-    drones[2].go_to_local_point(x=mission_point[3][0],
-                                y=mission_point[3][1],
-                                z=mission_point[3][2])
-    while not drones[2].point_reached():
-        pass
-    time.sleep(5)
-    drones[2].go_to_local_point(x=mission_point[4][0],
-                                y=mission_point[4][1],
-                                z=mission_point[4][2])
-    while not drones[2].point_reached():
-        pass
-    time.sleep(5)
-    drones[2].go_to_local_point(x=mission_point[5][0],
-                                y=mission_point[5][1],
-                                z=mission_point[5][2])
-    while not drones[2].point_reached():
-        pass
-    time.sleep(5)
-    drones[2].go_to_local_point(x=start_position_dron1[0],
-                                y=start_position_dron1[1],
-                                z=start_position_dron1[2])
-    while not drones[2].point_reached():
-        pass
-    drones[2].land()
-    drones[2].disarm()
 
-
-def dron2_task():
-    drones[3].arm()
-    drones[3].takeoff()
-    drones[3].go_to_local_point(x=mission_point[6][0],
-                                y=mission_point[6][1],
-                                z=mission_point[6][2])
-    while not drones[3].point_reached():
-        pass
-    time.sleep(5)
-    drones[3].go_to_local_point(x=mission_point[7][0],
-                                y=mission_point[7][1],
-                                z=mission_point[7][2])
-    while not drones[3].point_reached():
-        pass
-    time.sleep(5)
-    drones[3].go_to_local_point(x=mission_point[8][0],
-                                y=mission_point[8][1],
-                                z=mission_point[8][2])
-    while not drones[3].point_reached():
-        pass
-    time.sleep(5)
-    drones[3].go_to_local_point(x=mission_point[9][0],
-                                y=mission_point[9][1],
-                                z=mission_point[9][2])
-    while not drones[3].point_reached():
-        pass
-    time.sleep(5)
-    drones[3].go_to_local_point(x=start_position_dron2[0],
-                                y=start_position_dron2[1],
-                                z=start_position_dron2[2])
-    while not drones[3].point_reached():
-        pass
-    drones[3].land()
-    drones[3].disarm()
 
 if __name__ == '__main__':
+    drones[0].addPatrolPoint(mission_point[0])
+    drones[0].addPatrolPoint(mission_point[1])
+    drones[0].addPatrolPoint(mission_point[2])
+    drones[1].addPatrolPoint(mission_point[3])
+    drones[1].addPatrolPoint(mission_point[4])
+    drones[2].addPatrolPoint(mission_point[5])
+    drones[2].addPatrolPoint(mission_point[6])
+    drones[3].addPatrolPoint(mission_point[7])
+    drones[3].addPatrolPoint(mission_point[8])
+    drones[3].addPatrolPoint(mission_point[9])
+
+    drones[0].setEshelon(0.75)
+    drones[1].setEshelon(1.25)
+    drones[2].setEshelon(1.75)
+    drones[3].setEshelon(2.25)
+
+
     # Создаем поток для поиска пожаров и запускаем
-    dron1_thread = threading.Thread(target=dron1_task)
-    dron2_thread = threading.Thread(target=dron2_task)
-    dron2_thread.start()
-    time.sleep(2)
+    dron0_thread = threading.Thread(target=drones[0].startPatrol)
+    dron1_thread = threading.Thread(target=drones[1].startPatrol)
+    dron2_thread = threading.Thread(target=drones[2].startPatrol)
+    dron3_thread = threading.Thread(target=drones[3].startPatrol)
+
+    dron0_thread.start()
     dron1_thread.start()
+    dron2_thread.start()
+    dron3_thread.start()
+
+    # dron0_thread.join()
+    # dron1_thread.join()
+    # dron2_thread.join()
+    # dron3_thread.join()
 
 
